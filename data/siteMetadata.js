62d59072aaa83f24ffe7925030d86fbc49d86e8b
David Xavier <webxddesign@gmail.com>
2022-11-18 05:39:10 -0300

    Traducao v2

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -3,7 +3,7 @@ const siteMetadata = {
   author: 'David Xavier',
   headerTitle: 'XDPlansBlog',
   description: 'O blog da XD foi criado com Next.js e Tailwind.css',
-  language: 'en-us',
+  language: 'pt-br',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
